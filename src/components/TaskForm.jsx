7940912b98001e393ce440acf1a5575a79e55b97
unknown <polibos6366@gmail.com>
2024-09-16 17:28:35 -0600

    createContext

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
 const TaskForm = ({ createTask }) => {
+  const value = useContext(TaskContext);
+  console.log(value);
+
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,12 @@
+import { React, createContext } from "react";
+
+export const TaskContext = createContext();
+
+export const TaskContextProvider = (props) => {
+  let x = 20;
+  return (
+    <TaskContext.Provider value={x}>{props.children}</TaskContext.Provider>
+  );
+};
+
+export default TaskContext;
